db226a76c69003e11a13928b695a7b49f89bad2e
Kit <pinigins55@gmail.com>
2020-03-22 18:51:21 +0300

    Polish

--- src/main/kotlin/ru/kithome/deal_bot/Application.kt
+++ src/main/kotlin/ru/kithome/deal_bot/Application.kt
@@ -5,8 +5,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.telegram.telegrambots.ApiContextInitializer
 import org.telegram.telegrambots.meta.TelegramBotsApi
-import ru.kithome.deal_bot.bot.AbstractBot
 import ru.kithome.deal_bot.config.BotConfiguration
+import ru.kithome.deal_bot.service.DealBotService
 
 
 @SpringBootApplication
@@ -16,5 +16,6 @@ open class Application
 fun main(args: Array<String>) {
     ApiContextInitializer.init()
     val context = runApplication<Application>(*args)
-    context.getBeansOfType(AbstractBot::class.java).forEach { (_,v) -> TelegramBotsApi().registerBot(v)}
+    val dealBot = context.getBean(DealBotService::class.java)
+    TelegramBotsApi().registerBot(dealBot)
 }
